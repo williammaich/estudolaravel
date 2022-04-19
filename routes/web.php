65bb9ac6c20626fad8db65828ce78b547a15136a
williammaich <williammaich@hotmail.com>
2022-04-19 15:13:54 -0300

    Aula 01  - criação de index e a primeira rota

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+route::get('/users', [UserControllerntroller::class, 'index'])->name('users.index');
 
 Route::get('/', function () {
     return view('welcome');
